ea1b8c328c3e8c5b067aa766af7cd733319b22d4
Andrew Plaza <aplaza@liquidthink.net>
2018-09-05 13:18:26 -0400

    change blog link

--- src/components/Sidebar/About/index.js
+++ src/components/Sidebar/About/index.js
@@ -11,7 +11,7 @@ class About extends Component {
         <div className="bio">
           A CS Student who enjoys to write (occasionally) I often find projects to work on and contribute to, and more often leave project ideas sitting with the [private] tag in my <a className="link" href="https://github.com/InsidiousMind">Github</a>. I've attended a few hackathons with my club (Scranton Hacks), including HackPSU and DragonHacks (Drexel).
 
-      If I come across anything interesting in any of my projects, or am just excited about one, I'll usually write a post on my blog at <a className="link" href="https://code.liquidthink.net">Code.LiquidThink</a>
+      If I come across anything interesting in any of my projects, or am just excited about one, I'll usually write a post on my blog at <a className="link" href="https://blog.liquidthink.net">Blog.LiquidThink</a>
         </div>
       </div>
     )
